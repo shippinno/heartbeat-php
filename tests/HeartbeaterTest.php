da8727c5fab04c2e217564d27b9226fc6cf52e0f
Hirofumi Tanigami <tanigami@gmail.com>
2018-11-29 18:10:50 +0900

    Test  exceptions are ignored

--- tests/HeartbeaterTest.php
+++ tests/HeartbeaterTest.php
@@ -2,6 +2,7 @@
 
 namespace Shippinno\Heartbeat;
 
+use Exception;
 use Mockery;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use PHPUnit\Framework\TestCase;
@@ -30,6 +31,17 @@ class HeartbeaterTest extends TestCase
     {
         $heartbeater = Heartbeater::instance();
         $heart = Mockery::spy(Heart::class);
+        $heartbeater->setHeart($heart);
+        $heartbeater->setChannels(['CHANNEL' => 'SOME_CHANNEL']);
+        $heartbeater->heartbeat('CHANNEL');
+        $heart->shouldHaveReceived('beat')->once()->with('SOME_CHANNEL');
+    }
+
+    public function testHeartbeatIgnoringException()
+    {
+        $heartbeater = Heartbeater::instance();
+        $heart = Mockery::mock(Heart::class);
+        $heart->shouldReceive('beat')->andThrow(new Exception);
         $heartbeater->setHeart($heart);
         $heartbeater->setChannels(['CHANNEL' => 'SOME_CHANNEL']);
         $heartbeater->heartbeat('CHANNEL');
